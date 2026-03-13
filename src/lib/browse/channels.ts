17017a759d1986da243a180eae09ba7cb4ffa3bd
patrickkfkan <patrickkfkan@gmail.com>
2026-03-13 15:41:09 +0800

    Add support for browsing and playing individual episodes

--- src/lib/browse/channels.ts
+++ src/lib/browse/channels.ts
@@ -1,6 +1,7 @@
 import { type Display } from './types';
 import rp2 from '../RP2Context';
 import { type QueueItem } from '../playback/types';
+import { Channel } from '@patrickkfkan/rp.js';
 
 export async function getChannelsPage(): Promise<Display.Page> {
   const rpjs = rp2.getRpjsLib();
@@ -8,21 +9,38 @@ export async function getChannelsPage(): Promise<Display.Page> {
   const list: Display.List = {
     title: rp2.getI18n('RP2_CHANNELS'),
     items: channels.map((channel) => {
+      let qiUri: string;
+      let liUri: string;
+      let type: Display.ListItem['type'];
+      if (channel.isEpisodicRadio) {
+        qiUri = `rp2/episodes@channel=${encodeURIComponent(channel.id)}`;
+        type = 'folder';
+      }
+      else {
+        qiUri = `rp2/channel@id=${encodeURIComponent(channel.id)}`;
+        type = 'mywebradio';
+      }
       const qi = JSON.stringify({
         service: 'rp2',
-        uri: `rp2/channel@id=${encodeURIComponent(channel.id)}`,
+        uri: qiUri,
         name: channel.title,
         title: channel.title,
         artist: rp2.getI18n('RP2_RP'),
-        albumart: channel.images.default
+        albumart: channel.images.default ?? undefined
       } satisfies QueueItem);
+      if (channel.isEpisodicRadio) {
+        liUri = `rp2/episodes@channel=${encodeURIComponent(channel.id)}@qi=${encodeURIComponent(qi)}`;
+      }
+      else {
+        liUri = `rp2/channel@qi=${encodeURIComponent(qi)}`;
+      }
       const item: Display.ListItem = {
         service: 'rp2',
         title: channel.title,
         artist: rp2.getI18n('RP2_RP'),
-        type: 'mywebradio',
+        type,
         albumart: channel.images.default,
-        uri: `rp2/channel@qi=${encodeURIComponent(qi)}`
+        uri: liUri
       };
       return item;
     }),
@@ -35,5 +53,15 @@ export async function getChannelsPage(): Promise<Display.Page> {
       },
       lists: [list]
     }
+  };
+}
+
+function getEpisodicRadioItem(channel: Channel) {
+  return {
+    service: 'rp2',
+    uri: `rp2/episodes@channel=${encodeURIComponent(channel.id)}`,
+    title: channel.title,
+    artist: rp2.getI18n('RP2_RP'),
+    albumart: channel.images.default ?? undefined
   };
 }
